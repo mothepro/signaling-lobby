4315ff9af5240dd4ab02aec7510f2d428bea9018
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2019-11-28 12:01:43 -0800

    Transform a client into a buffer

--- src/messages.ts
+++ src/messages.ts
@@ -1,13 +1,18 @@
 import { Data } from 'ws'
-import { TextDecoder } from 'util'
+import { TextDecoder, TextEncoder } from 'util'
+import Client from './Client'
 
 export type Name = string
-export type LobbyID = number // unint32 (4 bytes)
-export type ClientID = number // unint16 (2 bytes)
+/** Uint32 (4 bytes) to represent the ID of a lobby. */
+export type LobbyID = number
+/** Uint16 (2 bytes) to represent the ID of a client. */
+export type ClientID = number
 
 const decode = new TextDecoder().decode
 
-export const getIntro = (input: Data): { name: string, lobby: number } => {
+const encode = new TextEncoder().encode
+
+export function getIntro(input: Data): { name: string, lobby: number } {
   if (input instanceof ArrayBuffer)
     return {
       lobby: new Uint32Array(input, 0, 1)[0],
@@ -16,8 +21,16 @@ export const getIntro = (input: Data): { name: string, lobby: number } => {
   throw TypeError(`Expected Introduction but  got ${input}`)
 }
 
-export const getId = (input: Data): ClientID => {
+export function getId(input: Data): ClientID {
   if (input instanceof ArrayBuffer && input.byteLength == 2)
     return new Uint16Array(input)[0]
   throw TypeError(`Expected ID but got ${input}`)
+}
+
+export function clientToBuffer(client: Client): ArrayBuffer {
+  const { id, name } = client,
+    ret = new Uint16Array(2 + Buffer.byteLength(name!, 'utf-8'))
+  ret.set([id])
+  ret.set(encode(name), 1)
+  return ret.buffer
 }

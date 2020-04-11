5b9b49883efbace7294e9f183d68edf59f0125fc
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2020-04-11 18:20:46 -0500

    Utilize ONLY Array buffers for transfering data

--- src/util/messages.ts
+++ src/util/messages.ts
@@ -1,26 +1,24 @@
 import { Data } from 'ws'
-import Client from '../Client'
 import stringSantizer from './stringSantizer'
-import { type } from 'os'
 
 export type Name = string
-/** Uint32 (4 bytes) to represent the ID of a lobby. */
+
+/** Int32 (4 bytes) to represent the ID of a lobby. */
 export type LobbyID = number
+
 /** Uint16 (2 bytes) to represent the ID of a client. */
 export type ClientID = number
 
-export type Intro = { name: Name, lobby: LobbyID }
-export type Proposal = { approval: true, ids: ClientID[] }
-export type Rejection = { approval: false, ids: ClientID[] }
-export type Reply = Proposal | Rejection
+/** Tell the server to add me to lobby with other potential clients. */
+export type Intro = {
+  /** Name to use as client. */
+  name: Name
+
+  /** ID of lobby to join */
+  lobby: LobbyID
+}
 
 export function getIntro(input: Data): { name: Name, lobby: LobbyID } {
-  if (typeof input == 'string') {
-    let { lobby, name } = JSON.parse(input)
-    lobby = parseInt(lobby)
-    name = stringSantizer(name)
-    return { lobby, name }
-  }
   if (input instanceof Buffer) // Array buffers are converted :(
     return {
       lobby: input.readInt32LE(0),
@@ -29,25 +27,26 @@ export function getIntro(input: Data): { name: Name, lobby: LobbyID } {
   throw TypeError(`Expected Introduction but got '${input}'`)
 }
 
+/** Update a group proposal. */
+export type Proposal = {
+  /** True to accept or create, False to reject. */
+  approve: boolean
+
+  /** Other clients to include in group. */
+  ids: Set<ClientID>
+}
+
 export function getProposal(input: Data): Proposal {
-  if (typeof input == 'string')
-    return JSON.parse(input)
-  // if (input instanceof ArrayBuffer && input.byteLength == 2)
-  //   return new Uint16Array(input)[0]
-  throw TypeError(`Expected ID but got '${input}'`)
-}
+  if (input instanceof Buffer && input.byteLength >= 3 && input.byteLength % 2 == 1) {
+    // Casting to a UInt16Array doesn't work due to `ws` prepending some values
+    const ids: Set<ClientID> = new Set
+    for (let offset = 1; offset < input.byteLength; offset += 2)
+      ids.add(input.readUInt16LE(offset))
 
-export function getId(input: Data): ClientID {
-  if (typeof input == 'string')
-    return JSON.parse(input)
-  if (input instanceof ArrayBuffer && input.byteLength == 2)
-    return new Uint16Array(input)[0]
+    return {
+      ids,
+      approve: !!input.readUInt8(0),
+    }
+  }
   throw TypeError(`Expected ID but got '${input}'`)
-}
-
-export function clientToBuffer(client: Client): Buffer {
-  const ret = Buffer.allocUnsafe(2 + client.name!.length)
-  ret.writeInt16LE(client.id!, 0)
-  ret.write(client.name!, 2)
-  return ret
 }

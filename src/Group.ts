f2bf6411c3b50d82db2fd7b2015c2905b17283b6
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-04-16 02:00:03 -0500

    Add helper test methods for building buffers

--- src/Group.ts
+++ src/Group.ts
@@ -40,9 +40,8 @@ export default class {
             // Only handle if group belongs to us, and only us
             if (ids.size == this.clients.size && ![...ids].filter(id => !this.clientIDs.has(id)).length)
               if (approve) {
-                logger(Level.INFO, client.id, '> joining group with', ids, 'acks (before)', this.acks)
-
                 this.acks.add(client.id)
+                logger(Level.INFO, client.id, '> joining group with', ids, 'and', this.acks, 'have agreed')
 
                 // Everyone is in!
                 if (this.acks.size == this.clients.size)
---
+++ test/util.ts
@@ -0,0 +1,18 @@
+import { LobbyID, ClientID } from '../src/messages'
+import { TextEncoder } from 'util'
+
+const encoder = new TextEncoder
+
+export function buildIntro(lobby: LobbyID, name: string) {
+  const buf = new DataView(new ArrayBuffer(4 + name.length))
+  buf.setInt32(0, lobby, true)
+  new Uint8Array(buf.buffer, 4).set(encoder.encode(name))
+  return buf.buffer
+}
+
+export function buildProposal(approve: boolean, ...ids: ClientID[]) {
+  const buf = new DataView(new ArrayBuffer(1 + 2 * ids.length))
+  buf.setInt8(0, +approve)
+  new Uint16Array(buf.buffer, 1).set(ids)
+  return buf.buffer
+}

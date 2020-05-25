3f1326c2b531e299a0d70d3c0ebd01d653669180
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-05-25 13:16:19 -0500

    Print the server version on initiation

--- bin/index.ts
+++ bin/index.ts
@@ -3,6 +3,7 @@ import { readFile } from 'fs'
 import { promisify } from 'util'
 import { createServer } from 'https'
 import { createServer as createUnsecureServer } from 'http'
+import { version } from '../package.json'
 import { verbose, hostname, port, maxLength, maxSize, maxConnections, idleTimeout, syncTimeout, key, cert } from './args'
 import logger, { Level, setLevel, logErr } from '../util/logger'
 import createSignalingLobby from '../src/createSignalingLobby'
@@ -21,7 +22,7 @@ try {
     : createUnsecureServer(),
     connection = await createSignalingLobby({ maxConnections, maxSize, maxLength, idleTimeout, syncTimeout }, server.listen(port, hostname))
 
-  logger(Level.USEFUL, 'Signaling server initiated', server.address())
+  logger(Level.USEFUL, version, 'Signaling Server listening', server.address())
 
   // Just wait through all connections
   for await (const _ of connection)
@@ -29,7 +30,7 @@ try {
 } catch (err) {
   logErr('An error occurred with the signaling server', err)
 } finally {
-  logger(Level.USEFUL, 'Shutting down the signaling server')
+  logger(Level.USEFUL, 'Shutting down the signaling server', version)
 }
 
 }())

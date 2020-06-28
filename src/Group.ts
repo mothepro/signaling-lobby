459c64939fdd3ceb58229e63cfdf5eaca3c48ffc
Maurice Prosper <mothepro@google.com>
2020-06-28 16:39:57 -0500

    Use better (not great) logic for grouping

--- src/Group.ts
+++ src/Group.ts
@@ -1,15 +1,15 @@
-import { SingleEmitter } from 'fancy-emitter'
 import { groupJoin, groupLeave, groupFinal } from './outgoing'
 import Client, { State } from './Client'
-import logger, { Level } from '../util/logger'
+import logger, { Level, logErr } from '../util/logger'
 import { Max, ClientID } from '../util/constants'
 
-class LeaveError extends Error {
-  constructor(
-    /** ID of the client who causes group to close. */
-    readonly id: ClientID,
-    message?: string
-  ) { super(message) }
+const enum GroupState {
+  /** The group is waiting for everyone to accept. */
+  PENDING,
+  /** Everyone in the group accepted. */
+  FINALIZED,
+  /** Someone in the group rejected. */
+  CLOSED,
 }
 
 export default class Group {
@@ -35,17 +35,10 @@ export default class Group {
 
   private readonly clients: Map<ClientID, Client> = new Map
 
+  private groupState = GroupState.PENDING
+
   /** Code to be sent to the clients when group is done. */
   private readonly code = Math.trunc(Math.random() * Max.INT)
-
-  private readonly ready = new SingleEmitter(() => {
-    logger(Level.INFO, ...this.clients.keys(), 'have finalized a group')
-    for (const [id, { send, stateChange }] of this.clients) {
-      stateChange.activate(State.SYNCING)
-      // Browser doesn't know their own id, but we give it to them first as a comparator.
-      send(groupFinal(this.code, id, ...this.idsWithout(id)))
-    }
-  })
 
   /** All the client IDs in this group without `ids`. */
   private idsWithout(...ids: ClientID[]) {
@@ -60,13 +53,38 @@ export default class Group {
     logger(Level.DEBUG, ackerId, '> joining group of', ...this.clients.keys(), 'so far', this.acks, 'have agreed')
 
     // Everyone is in!
-    if (this.acks.size == this.clients.size)
-      this.ready.activate()
-    else
+    if (this.acks.size == this.clients.size) {
+      if (this.groupState == GroupState.PENDING) {
+        this.groupState = GroupState.FINALIZED
+        logger(Level.INFO, ...this.clients.keys(), 'have finalized a group')
+
+        for (const [id, client] of this.clients) {
+          client.stateChange.activate(State.SYNCING)
+          this.bindMessage(client)
+          // Browser doesn't know their own id, but we give it to them first as a comparator.
+          client.send(groupFinal(this.code, id, ...this.idsWithout(id)))
+        }
+        Group.groups.delete(Group.hashIds(...this.clients.keys()))
+      } else
+        logger(Level.DEBUG, 'Can not finalize group of', ...this.clients.keys(), 'since it is already', this.groupState)
+    } else
       for (const [id, { send }] of this.clients)
         if (id != ackerId)
           // ackr will go first & browser doesn't know their own id
           send(groupJoin(ackerId, ...this.idsWithout(ackerId, id)))
+  }
+
+  /** Shuts down this group due to `client`'s `reason`. */
+  private close({ id }: Client, reason: string) {
+    if (this.groupState == GroupState.PENDING) {
+      this.groupState = GroupState.CLOSED
+      logErr(id, reason, 'potential group', ...this.clients.keys())
+      for (const [other, { send }] of this.clients)
+        if (id != other) // ackr will go first & browser doesn't know their own id
+          send(groupLeave(id, ...this.idsWithout(other, id)))
+      Group.groups.delete(Group.hashIds(...this.clients.keys()))
+    } else
+      logger(Level.DEBUG, 'Can not close group of', ...this.clients.keys(), 'since it is already', this.groupState)
   }
 
   /** Create and clean up a group once it is no longer needed. */
@@ -79,21 +97,10 @@ export default class Group {
     for (const [, client] of this.clients) {
       this.bindState(client)
       this.bindProposal(client)
-      this.bindMessage(client)
     }
 
     logger(Level.DEBUG, initiator.id, '> proposed group to include', ...this.clients.keys())
     this.ack(initiator.id)
-
-    // Notify all other clients when a group fails.
-    this.ready.event
-      .catch((e: LeaveError) => {
-        for (const [id, { send }] of this.clients)
-          if (id != e.id)
-            // ackr will go first & browser doesn't know their own id
-            send(groupLeave(e.id, ...this.idsWithout(e.id, id)))
-      })
-      .finally(() => Group.groups.delete(Group.hashIds(...this.clients.keys())))
   }
 
   private async bindProposal(client: Client) {
@@ -104,32 +111,34 @@ export default class Group {
         if (approve)
           this.ack(client.id)
         else
-          this.ready.deactivate(new LeaveError(client.id, `${client.id} doesn't want to join ${[...ids]}`))
-    }
-  }
-
-  private async bindMessage(client: Client) {
-    try {
-      for await (const { to, content } of client.message) {
-        // broadcast to "self" means to actually broadcast all group members (excluding self) if group is activated
-        if (to == client.id && !client.stateChange.isAlive) {
-          for (const [id, { send }] of this.clients)
-            if (to != id)
-              send(content.buffer)
-        } else { // transform the `to` into a `from` and ship it to the `from` client
-          content.setUint16(0, client.id, true)
-          this.clients.get(to)?.send(content.buffer)
-        }
-      }
-    } catch (err) {
-      client.stateChange.deactivate(err)
+          this.close(client, 'does not want to join')
+      
+      // Do not need to keep listening for proposals
+      if (this.groupState != GroupState.PENDING) 
+        return
     }
   }
 
   private async bindState(client: Client) {
     try { // Idle until client is dead so it can be GC'd.
-      for await (const _ of client.stateChange);
+      for await (const state of client.stateChange)
+        if (state == State.SYNCING)
+          break
     } catch { } // Handled in Client's constructor
-    this.ready.deactivate(new LeaveError(client.id, `${client.id} disconnected while in potential group with ${[...this.clients.keys()]}`))
+    this.close(client, 'disconnected from')
+  }
+
+  private async bindMessage(client: Client) {
+    for await (const { to, content } of client.message) {
+      // broadcast to "self" means to actually broadcast all group members (excluding self) if group is activated
+      if (to == client.id) {
+        for (const [id, { send }] of this.clients)
+          if (to != id)
+            send(content.buffer)
+      } else { // transform the `to` into a `from` and ship it to the `from` client
+        content.setUint16(0, client.id, true)
+        this.clients.get(to)?.send(content.buffer)
+      }
+    }
   }
 }
